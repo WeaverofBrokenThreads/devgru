f93bad0a4913f76689a77c3877f3007d3752e06c
Weaver of Broken Threads <Ashflamefestivals@gmail.com>
2016-06-30 18:37:03 -0400

    Maneuvers and Technique support

--- source/combat.cpp
+++ source/combat.cpp
@@ -6,3 +6,29 @@ Combat::Combat(vector<Character *> Fighter)
 {
 
 }
+
+
+// Stick with struct for now, maybe change to class later.
+
+struct Maneuver
+{
+    Maneuver();
+
+    // VARS
+
+    string type;
+    float InitiativeMod;
+    float AttackMod;
+    float DefenseMod;
+    float PrepareTime;
+    float BaseTime;
+    float MSMod;
+    vector<Technique> Techniques;
+
+};
+
+// Same as Maneuver
+struct Technique
+{
+
+};
